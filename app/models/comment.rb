89d4891d4f0049bfaa92c0a6137654a59b61ab6c
Ryuukhi <maatheusviidal@hotmail.com>
2021-11-28 15:31:01 -0300

    added comments validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+  validates :title, length: { in: 4..30 }
+  validates :body, presence: true, length: { maximum: 500 }
+  validates :post, presence: true
+  validates :user, presence: true
+
   belongs_to :post, foreign_key: 'post_id'
   belongs_to :user, foreign_key: 'user_id'
 end
